3be19eb64696a791445670cd7c8a24f88e9a1521
kunapand_cisco <kunapand@cisco.com>
2026-05-01 17:38:44 +0530

    Initial commit: Spring Boot project setup

---
+++ src/test/java/com/flightbooking/FlightBookingApiApplicationTests.java
@@ -0,0 +1,12 @@
+package com.flightbooking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlightBookingApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
